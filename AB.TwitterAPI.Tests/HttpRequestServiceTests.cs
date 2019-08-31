508ee57ddcbbb8b9fd9639bf349872b7536f4c30
aaronmbos <aaronmbos@gmail.com>
2019-08-30 21:38:31 -0500

    Add Service Tests

--- AB.TwitterAPI.Tests/HttpRequestServiceTests.cs
+++ AB.TwitterAPI.Tests/HttpRequestServiceTests.cs
@@ -17,6 +17,14 @@ namespace AB.TwitterAPI.Tests
         }
 
         [Test]
+        public async void GetAsync_WithBaseAddress_ReturnsResponse()
+        {
+            IHttpRequestService httpReqSvc = new HttpRequestService(new HttpClient());
+            object resp = await httpReqSvc.GetAsync(new Uri("https://google.com"), string.Empty, null);
+            Assert.IsNotNull(resp);
+        }
+
+        [Test]
         public void GetAsync_WithNullUrl_ThrowsArgNullExc()
         {
             IHttpRequestService httpReqSvc = new HttpRequestService(new HttpClient());
